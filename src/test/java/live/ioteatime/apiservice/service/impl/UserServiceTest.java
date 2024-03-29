5cbccaf58cf64b1f541d409adcf24a346034e302
haedanui <rhs9705kr@naver.com>
2024-03-29 18:35:16 +0900

    FIX : 컨트롤러 테스트 코드 돌아가도록 함수 수정 @RequestHeader를 사용하여 X_USER_ID를 받아와야 하지만 현재 테스트 코드는 X_USER_ID를 사용하지 않으므로 임시로 비활성화시킴

--- src/test/java/live/ioteatime/apiservice/service/impl/UserServiceTest.java
+++ src/test/java/live/ioteatime/apiservice/service/impl/UserServiceTest.java
@@ -3,15 +3,16 @@ package live.ioteatime.apiservice.service.impl;
 import live.ioteatime.apiservice.domain.User;
 import live.ioteatime.apiservice.dto.UserDto;
 import live.ioteatime.apiservice.repository.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -19,6 +20,9 @@ class UserServiceTest {
 
     @Mock
     UserRepository userRepository;
+
+    @Mock
+    BCryptPasswordEncoder passwordEncoder;
 
     @InjectMocks
     UserServiceImpl userService;
@@ -39,12 +43,25 @@ class UserServiceTest {
 
         UserDto response = userService.loadUserByUserName(id);
 
-        Assertions.assertThat(response.getId()).isEqualTo(id);
+        assertThat(response.getId()).isEqualTo(id);
         //이 테스트를 통과시키기 위해서 userGetDto에 setter를 넣었음
         //BeanUtils는 getter와 setter 메서드를 통해 값을 복사하기 때문임
     }
 
     @Test
     void createUser() {
+        UserDto userDto = new UserDto();
+        userDto.setId("ryu");
+        userDto.setPassword("password");
+        userDto.setName("seungjin");
+
+        User user = new User();
+        user.setId("ryu");
+        user.setPassword("password");
+        user.setName("seungjin");
+
+        given(userRepository.save(user)).willReturn(user);
+        String saveUser = userService.createUser(userDto);
+        assertThat("ryu").isEqualTo(saveUser);
     }
 }
