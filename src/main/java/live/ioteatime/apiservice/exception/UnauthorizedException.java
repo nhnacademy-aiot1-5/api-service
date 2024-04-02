f284216fd887f0b45cc846c7bb26794ede175fd1
july433 <july433@naver.com>
2024-04-02 16:23:24 +0900

    fix : 수정

--- src/main/java/live/ioteatime/apiservice/exception/UnauthorizedException.java
+++ src/main/java/live/ioteatime/apiservice/exception/UnauthorizedException.java
@@ -2,6 +2,6 @@ package live.ioteatime.apiservice.exception;
 
 public class UnauthorizedException extends RuntimeException {
     public UnauthorizedException() {
-        super("UnauthorizedException");
+        super("UnauthorizedException : 권한이 없는 접근입니다.");
     }
 }
