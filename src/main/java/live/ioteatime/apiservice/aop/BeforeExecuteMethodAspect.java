171dcac9813c509191e940af0ceab71ea64a35d3
july433 <july433@naver.com>
2024-04-04 15:02:07 +0900

    add : csrf 방어를 위해 referer header 비교 aop 추가

--- src/main/java/live/ioteatime/apiservice/aop/BeforeExecuteMethodAspect.java
+++ src/main/java/live/ioteatime/apiservice/aop/BeforeExecuteMethodAspect.java
@@ -1,14 +1,12 @@
 package live.ioteatime.apiservice.aop;
 
-import com.sun.net.httpserver.Headers;
 import live.ioteatime.apiservice.domain.Role;
+import live.ioteatime.apiservice.exception.RefererNotMatchesException;
 import live.ioteatime.apiservice.exception.UnauthorizedException;
 import live.ioteatime.apiservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -19,11 +17,11 @@ import java.util.Objects;
 @Aspect
 @Component
 @RequiredArgsConstructor
-public class RoleCheckAspect {
+public class BeforeExecuteMethodAspect {
     private final UserRepository userRepository;
 
     @Before("@annotation(live.ioteatime.apiservice.annotation.AdminOnly)")
-    public void doRoleCheck() {
+    public void hasAdminRole() {
         HttpServletRequest httpServletRequest =
                 ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
 
@@ -31,6 +29,26 @@ public class RoleCheckAspect {
 
         if (!role.equals(Role.ADMIN)) {
             throw new UnauthorizedException();
+        }
+    }
+
+    @Before("@annotation(org.springframework.web.bind.annotation.PostMapping) || " +
+            "@annotation(org.springframework.web.bind.annotation.PutMapping) || " +
+            "@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    public void areRefererAndHeaderSame() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+        if (attributes == null) {
+            throw new IllegalStateException("ServletRequestAttributes 또는 요청이 존재하지 않습니다.");
+        }
+
+        HttpServletRequest request = attributes.getRequest();
+
+        String referer = request.getHeader("referer");
+        String host = request.getHeader("host");
+
+        if (!host.equals(referer)) {
+            throw new RefererNotMatchesException("referer가 host와 일치하지 않습니다." + "referer : " + referer + " host : " + host);
         }
     }
 }
