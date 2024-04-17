e663102361ec4a79ceef3d754f8425d5c9f1c073
haedanui <rhs9705kr@naver.com>
2024-04-17 22:08:54 +0900

    feat : 조직 예산 수정 컨트롤러 구현

--- src/main/java/live/ioteatime/apiservice/controller/AdminController.java
+++ src/main/java/live/ioteatime/apiservice/controller/AdminController.java
@@ -4,8 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import live.ioteatime.apiservice.annotation.AdminOnly;
 import live.ioteatime.apiservice.domain.Sensor;
+import live.ioteatime.apiservice.dto.OrganizationDto;
 import live.ioteatime.apiservice.dto.UserDto;
 import live.ioteatime.apiservice.service.AdminService;
+import live.ioteatime.apiservice.service.OrganizationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,7 @@ import java.util.List;
 @Tag(name = "ADMIN 컨트롤러", description = "어드민만 사용할 수 있는 컨트롤러입니다.")
 public class AdminController {
     private final AdminService adminService;
+    private final OrganizationService organizationService;
     private final String X_USER_ID = "X-USER-ID";
 
     /**
@@ -42,6 +45,13 @@ public class AdminController {
         return ResponseEntity.ok(adminService.getUsers());
     }
 
+    @GetMapping("/budget")
+    @AdminOnly
+    @Operation(summary = "조직의 현재 설정 금액을 가져오는 API", description = "조직의 현재 설정금액을 가져옵니다.")
+    public ResponseEntity<OrganizationDto> getBudget(@RequestHeader(X_USER_ID) String userId) {
+        return ResponseEntity.ok(organizationService.getOrganization(userId));
+    }
+
     /**
      * 어드민만 사용할 수 있는 명령어이며 연결된 센서의 리스트를 가져오는 컨트롤러다.
      * @return 연결된 센서들의 리스트를 반환한다.
@@ -62,7 +72,15 @@ public class AdminController {
     @PutMapping("/roles")
     @AdminOnly
     @Operation(summary = "유저 권한을 수정하는 API", description = "ADMIN 유저가 GUEST 권한을 가진 유저의 권한을 GUEST에서 USER로 수정합니다.")
-    public ResponseEntity<UserDto> updateUserRole(@RequestHeader(X_USER_ID) String loginUserId, String userId){
+    public ResponseEntity<UserDto> updateUserRole(String userId){
         return ResponseEntity.ok(adminService.updateUserRole(userId));
+    }
+
+
+    @PutMapping("/budget")
+    @AdminOnly
+    @Operation(summary = "어드민이 속한 조직의 요금을 설정하는 API", description = "어드민이 속한 조직의 요금을 설정합니다.")
+    public ResponseEntity<OrganizationDto> updateBudget(@RequestHeader(X_USER_ID) String userId, Long budget){
+        return ResponseEntity.ok(organizationService.updateBudget(userId, budget));
     }
 }
