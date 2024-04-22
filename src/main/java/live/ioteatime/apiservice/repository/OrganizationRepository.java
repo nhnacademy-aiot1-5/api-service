f1d586b3969894979682d4c2a78add4ff9f09f29
sylim <sammys27@naver.com>
2024-04-22 14:57:09 +0900

    fix : 회원가입 로직 수정
    기존 로직은 회원가입시 조직이름으로 조직을 검색합니다.
    조직 이름은 중복될 수 있기 때문에,
    조직 코드로 조직을 검색 하고,
    조직 이름과 코드 일치 여부를 판단하도록 수정하였습니다.

--- src/main/java/live/ioteatime/apiservice/repository/OrganizationRepository.java
+++ src/main/java/live/ioteatime/apiservice/repository/OrganizationRepository.java
@@ -4,5 +4,5 @@ import live.ioteatime.apiservice.domain.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
-    Organization findByName(String name);
+    Organization findByOrganizationCode(String organizationCode);
 }
