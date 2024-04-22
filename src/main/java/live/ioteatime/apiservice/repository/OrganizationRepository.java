22e5925edb8a583386ad73317bdad5e433c69fd2
codeTheStudent <hjun303@gnu.ac.kr>
2024-04-22 14:35:24 +0900

    feat : 시간별 전력량 구하는 DailyElectricityServiceImpl.java 구현
    mysql에 저장된 월별 데이터 및 금일 시간 별 데이터 가져오는 로직 구현완료

--- src/main/java/live/ioteatime/apiservice/repository/OrganizationRepository.java
+++ src/main/java/live/ioteatime/apiservice/repository/OrganizationRepository.java
@@ -3,6 +3,10 @@ package live.ioteatime.apiservice.repository;
 import live.ioteatime.apiservice.domain.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
     Organization findByName(String name);
+
+    Optional<Organization> findById(int id);
 }
