bf922f33b02dbf661ff672bc24e8ac350afe800a
haedanui <rhs9705kr@naver.com>
2024-05-01 18:30:01 +0900

    delete: 안쓰는 파일 삭제

--- src/main/java/live/ioteatime/apiservice/response/ResultResponse.java
+++
@@ -1,9 +0,0 @@
-package live.ioteatime.apiservice.response;
-
-public class ResultResponse {
-    private String result;
-
-    public ResultResponse(String result) {
-        this.result = result;
-    }
-}
