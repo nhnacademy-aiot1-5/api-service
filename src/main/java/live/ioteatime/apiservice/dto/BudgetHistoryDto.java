84a2175ae7ff222712f5b0ab179ff49fee78c1c0
sylim <sammys27@naver.com>
2024-05-03 17:20:08 +0900

    refactor : JsonProperty 추가

--- src/main/java/live/ioteatime/apiservice/dto/BudgetHistoryDto.java
+++ src/main/java/live/ioteatime/apiservice/dto/BudgetHistoryDto.java
@@ -1,5 +1,6 @@
 package live.ioteatime.apiservice.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
@@ -8,11 +9,13 @@ public interface BudgetHistoryDto {
     @Schema(description = "아이디입니다.")
     int getId();
 
+    @JsonProperty("change_time")
     LocalDateTime getChangeTime();
 
     Long getBudget();
 
     interface OrganizationDto {
+
         int getId();
 
         String getName();
