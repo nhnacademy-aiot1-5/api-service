6e1d4dd76550b1f49ac92c2d7e75ad46bf3d0474
dmswl <siddltkfkd@gmail.com>
2024-05-09 18:39:42 +0900

    feat: 조직 id에 따른 실시간 전력량(w)을 구하는 로직 구현
    리턴타입은 (장소, 타입, w)의 리스트

---
+++ src/main/java/live/ioteatime/apiservice/controller/RealtimeElectricityController.java
@@ -0,0 +1,24 @@
+package live.ioteatime.apiservice.controller;
+
+import live.ioteatime.apiservice.dto.electricity.RealtimeElectricityResponseDto;
+import live.ioteatime.apiservice.service.RealtimeElectricityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/realtime")
+@RequiredArgsConstructor
+public class RealtimeElectricityController {
+    private final RealtimeElectricityService realtimeElectricityService;
+
+    @GetMapping("electricity")
+    public ResponseEntity<List<RealtimeElectricityResponseDto>> getRealtimeElectricity(@RequestParam int organizationId){
+        return ResponseEntity.ok(realtimeElectricityService.getRealtimeElectricity(organizationId));
+    }
+}
