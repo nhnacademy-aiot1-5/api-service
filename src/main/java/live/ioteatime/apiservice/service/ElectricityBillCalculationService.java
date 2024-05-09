4832fdb0ea2017ce63b7d7640df7217f94196437
huhsiyoung <july433@naver.com>
2024-05-09 16:43:27 +0900

    add : 메서드 추가

--- src/main/java/live/ioteatime/apiservice/service/ElectricityBillCalculationService.java
+++ src/main/java/live/ioteatime/apiservice/service/ElectricityBillCalculationService.java
@@ -2,8 +2,16 @@ package live.ioteatime.apiservice.service;
 
 public interface ElectricityBillCalculationService {
     Long calculateElectricityBill(Long kwh);
+
     Long getGeneralCharge();
+
     Long getDemandCharge();
+
     Long getClimateChangeCharge(Long kwh);
+
     Long getFuelCostAdjustmentCharge(Long kwh);
+
+    Long getVAT(Long electricityBill);
+
+    Long getElectricityIndustryInfraFund(Long electricityBill);
 }
