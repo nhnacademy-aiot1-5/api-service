61bc8e3e96b3f98aae5ed72981dd92db2f006845
sylim <sammys27@naver.com>
2024-05-11 19:29:38 +0900

    refactor : dto 이름 변경
    PreciseKwhResponseDto -> PreciseElectricityResponseDto로 변경

--- src/main/java/live/ioteatime/apiservice/service/HourlyElectricityService.java
+++ src/main/java/live/ioteatime/apiservice/service/HourlyElectricityService.java
@@ -1,9 +1,9 @@
 package live.ioteatime.apiservice.service;
 
-import live.ioteatime.apiservice.dto.electricity.PreciseKwhResponseDto;
+import live.ioteatime.apiservice.dto.electricity.PreciseElectricityResponseDto;
 
 import java.util.List;
 
 public interface HourlyElectricityService {
-    List<PreciseKwhResponseDto> getOneHourTotalElectricties(int organizationId);
+    List<PreciseElectricityResponseDto> getOneHourTotalElectricties(int organizationId);
 }
