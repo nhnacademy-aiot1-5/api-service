7a991e2e2c7b5c01d3ab6bc086d12c3439ff6acb
huhsiyoung <july433@naver.com>
2024-05-11 10:27:16 +0900

    fix : 전력 요금 계산 서비스 자바독 @param 설명 추가, 전력 사용량 Double 타입으로 변경

--- src/main/java/live/ioteatime/apiservice/service/ElectricityBillCalculationService.java
+++ src/main/java/live/ioteatime/apiservice/service/ElectricityBillCalculationService.java
@@ -5,7 +5,7 @@ public interface ElectricityBillCalculationService {
     /**
      * 전력요금을 계산하고 청구요금을 반환하는 메서드입니다.
      * <br>
-     * 요금 계산 중 소수점 이하는 버림 처리합니다.
+     * 요금 계산 결과 소수점 이하는 버림 처리합니다.
      * <br>
      * 전력요금(electricityBill) = 기본요금 + 전력량요금 + 기후변화요금 + 연료비조정요금
      * <br>
@@ -13,7 +13,7 @@ public interface ElectricityBillCalculationService {
      * <br>
      * @return 청구요금(billingCharge)을 반환합니다.
      */
-    Long calculateElectricityBill(Long kwh);
+    Long calculateElectricityBill(Double thisMonthKwhUsage);
 
     /**
      * 기본요금을 반환하는 메서드입니다.
@@ -22,32 +22,37 @@ public interface ElectricityBillCalculationService {
     Long getGeneralCharge();
 
     /**
-     * 전력량요금(사용한 전력에 대한 요금)을 반환하는 메서드입니다.
-     * @return 30일치 전력량요금을 반환합니다.
+     * 이번 달 사용한 전력에 대한 요금을 반환하는 메서드입니다.
+     * @param thisMonthKwhUsage 이번 달 전력 사용량입니다.
+     * @return 이번 달 전력량요금을 반환합니다.
      */
-    Long getDemandCharge(Long kwh);
+    Long getDemandCharge(Double thisMonthKwhUsage);
 
     /**
      * 기후변화요금을 반환하는 메서드입니다.
+     * @param thisMonthKwhUsage 이번 달 전력 사용량입니다.
      * @return 기후변화요금을 반환합니다.
      */
-    Long getClimateChangeCharge(Long kwh);
+    Long getClimateChangeCharge(Double thisMonthKwhUsage);
 
     /**
      * 연료비조정요금을 반환하는 메서드입니다.
+     * @param thisMonthKwhUsage 이번 달 전력 사용량입니다.
      * @return 연료비조정요금을 반환합니다.
      */
-    Long getFuelCostAdjustmentCharge(Long kwh);
+    Long getFuelCostAdjustmentCharge(Double thisMonthKwhUsage);
 
     /**
      * 전력요금의 10%인 부가가치세를 반환하는 메서드입니다.
+     * @param electricityBill 이번 달 전력요금입니다.
      * @return 부가가치세를 반환합니다.
      */
-    Long getVAT(Long electricityBill);
+    Long getVAT(long electricityBill);
 
     /**
      * 전력요금의 3.7%인 전력산업발전기금을 반환하는 메서드입니다.
+     * @param electricityBill 이번 달 전력요금입니다.
      * @return 전력산업기반기금을 반환합니다.
      */
-    Long getElectricityIndustryInfraFund(Long electricityBill);
+    Long getElectricityIndustryInfraFund(long electricityBill);
 }
