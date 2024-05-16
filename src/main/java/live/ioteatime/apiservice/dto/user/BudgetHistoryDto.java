df815d6e47bf6e67d905f866776bfc4e9df798f1
sylim <sammys27@naver.com>
2024-05-16 15:07:05 +0900

    refactor : BudgetHistoryDto 인터페이스에서 클래스로 변경

---
+++ src/main/java/live/ioteatime/apiservice/dto/user/BudgetHistoryDto.java
@@ -0,0 +1,17 @@
+package live.ioteatime.apiservice.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class BudgetHistoryDto {
+    int id;
+    @JsonProperty("change_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    LocalDateTime changeTime;
+    Long budget;
+}
