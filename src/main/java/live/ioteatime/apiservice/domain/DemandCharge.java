430662231d7cc28929a271724b7e7544444004d2
huhsiyoung <july433@naver.com>
2024-05-23 14:14:32 +0900

    fix : 계절별 요금 수정

--- src/main/java/live/ioteatime/apiservice/domain/DemandCharge.java
+++ src/main/java/live/ioteatime/apiservice/domain/DemandCharge.java
@@ -2,14 +2,13 @@ package live.ioteatime.apiservice.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 public enum DemandCharge {
-    SPRING_FALL(130.3),
-    SUMMER(98.6),
-    WINTER(142.6);
+    SPRING_FALL(98.6),
+    SUMMER(142.6),
+    WINTER(130.3);
 
     private final double demandCharge;
 }
