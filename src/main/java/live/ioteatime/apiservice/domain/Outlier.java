07d370329eb803b46b218035dad4aa65641cd22b
codeTheStudent <hjun303@gnu.ac.kr>
2024-05-26 22:49:24 +0900

    fix : outlier auto increment 설정

--- src/main/java/live/ioteatime/apiservice/domain/Outlier.java
+++ src/main/java/live/ioteatime/apiservice/domain/Outlier.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "outlier")
@@ -16,6 +13,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 public class Outlier {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "outlier_id")
     int id;
     @Column
