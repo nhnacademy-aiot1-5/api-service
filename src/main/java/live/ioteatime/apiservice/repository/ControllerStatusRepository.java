ec43956c956c2b82e6dbebbd0f7d80034338e2e3
codeTheStudent <hjun303@gnu.ac.kr>
2024-05-28 23:17:07 +0900

    fix : ControllerStatus 제거

--- src/main/java/live/ioteatime/apiservice/repository/ControllerStatusRepository.java
+++
@@ -1,7 +0,0 @@
-package live.ioteatime.apiservice.repository;
-
-import live.ioteatime.apiservice.domain.ControllerStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ControllerStatusRepository extends JpaRepository<ControllerStatus, String> {
-}
